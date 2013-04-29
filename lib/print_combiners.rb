63fb4ae50aeadf18311649be7f1faca26331ebb5
Dominic Sisneros <dsisnero@gmail.com>
2013-04-29 15:50:02 -0600

    initial commit

---
+++ lib/print_combiners.rb
@@ -0,0 +1,3 @@
+module PrintCombiners
+  VERSION = '0.0.1'
+end
---
+++ spec/print_combiners_spec.rb
@@ -0,0 +1,9 @@
+require_relative 'spec_helper'
+
+require "print_combiners"
+
+describe PrintCombiners do
+ it "can be created" do
+   PrintCombiners.new.must_be_instance_of PrintCombiners
+ end
+end
